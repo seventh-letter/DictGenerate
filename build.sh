5093c6cd3c14ef26e8c850c9e449d6064114b001
telan <ziiber@foxmail.com>
2019-03-20 13:33:07 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build DictGenerate.go
+mv DictGenerate DictGenerate_linux_amd64
+
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build DictGenerate.go
+mv DictGenerate.exe DictGenerate_windows_amd64.exe
