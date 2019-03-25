6301f3e4cbe60de45e85551ac3c992efc2a4cfc8
telan <ziiber@foxmail.com>
2019-03-25 14:06:46 +0800

    fix build

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+go build DictGenerate.go
+mv DictGenerate DictGenerate_darwin_amd64
+
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build DictGenerate.go
 mv DictGenerate DictGenerate_linux_amd64
 
